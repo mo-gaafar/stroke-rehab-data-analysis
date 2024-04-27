25d03fbbee9b68206107656c98ac65d4cd3cc964
Mohamed Nasser <mohamednasser2001@hotmail.com>
2024-04-27 20:52:44 +0300

    Refactor data loading and processing in load_mne.py

--- scripts/load_mne.py
+++ scripts/load_mne.py
@@ -3,15 +3,21 @@ from mne.io import RawArray
 from mne import create_info
 import mne
 
+
 def mne_load_data(data_dict):
     # Load into MNE format
 
+    mne_data_dict = {
+
+    }
     # for every patient, pre and post, train and test
     for patient in data_dict.keys():
         for session in data_dict[patient].keys():
             for task in data_dict[patient][session].keys():
+
                 eeg = data_dict[patient][session][task]['y'].T
-                sfreq = data_dict[patient][session][task]['fs'].squeeze().item()
+                sfreq = data_dict[patient][session][task]['fs'].squeeze(
+                ).item()
                 triggers = data_dict[patient][session][task]['trig']
 
                 print(f"Patient: {patient}, Session: {session}, Task: {task}")
@@ -21,11 +27,18 @@ def mne_load_data(data_dict):
                     f"Trigger data shape: {data_dict[patient][session][task]['trig'].shape}")
 
                 # Create info object
-                info = create_info(ch_names=[f"EEG {i}" for i in range(eeg.shape[0])],
-                                sfreq=sfreq)
+                channel_map = {
+                    1: "FC3", 2: "FCz", 3:"FC4",
+                    4:"C5", 5:"C3", 6:"C1", 7:"Cz", 8:"C2", 9:"C4", 10:"C6",
+                    11: "CP3", 12:"CP1", 13:"CPz", 14:"CP2", 15:"CP4",
+                    16:"Pz",
+                }
+                info = create_info(ch_names=[channel_map[i+1] for i in range(eeg.shape[0])],
+                                   sfreq=sfreq, ch_types=['eeg']*eeg.shape[0])
 
                 # Create RawArray object
                 raw = RawArray(eeg, info)
+                raw.set_montage(mne.channels.make_standard_montage('standard_1020'))
 
                 # Create events array
                 # events_raw = data_dict[patient][session][task]['trig'].reshape(
@@ -33,14 +46,16 @@ def mne_load_data(data_dict):
                 # events = np.array([[i, 0, int(events_raw[i])]
                 #                 for i in range(events_raw.shape[0])])
                 triggerd = np.zeros_like(triggers, dtype=int)
-                triggerd[1:] = ((triggers[1:]-triggers[:-1])!=0) * (triggers[1:]!=0)
-                triggerd[triggerd!=0] = triggers[triggerd!=0]
-                events = np.column_stack((np.argwhere(triggerd)[:,0], np.zeros(sum(triggerd!=0), dtype=int),triggerd[triggerd!=0]))
+                triggerd[1:] = ((triggers[1:]-triggers[:-1])
+                                != 0) * (triggers[1:] != 0)
+                triggerd[triggerd != 0] = triggers[triggerd != 0]
+                events = np.column_stack((np.argwhere(triggerd)[:, 0], np.zeros(
+                    sum(triggerd != 0), dtype=int), triggerd[triggerd != 0]))
 
                 # Mapping of events to ids
                 event_dict = {
-                    "left" : 1,
-                    "right" : -1,
+                    "left": 1,
+                    "right": -1,
                 }
 
                 # divide the eeg into epochs, each epoch is 8 seconds long (trigger is at 2 seconds, at 3.5 seconds )
@@ -65,10 +80,19 @@ def mne_load_data(data_dict):
 
                 # Save epochs
                 left_epochs.save(
-                    f"data/stroke/{patient}_{session}_{task}_left-epo.fif")
+                    f"data/stroke/{patient}_{session}_{task}_left-epo.fif", overwrite=True)
                 right_epochs.save(
-                    f"data/stroke/{patient}_{session}_{task}_right-epo.fif")
+                    f"data/stroke/{patient}_{session}_{task}_right-epo.fif", overwrite=True)
 
                 print("\n")
 
-    return 
+                mne_data_dict[patient] = {
+                    session: {
+                        task: {
+                            "left": left_epochs,
+                            "right": right_epochs,
+                        }
+                    }
+                }
+
+    return mne_data_dict
---
+++ scripts/spectro.py
@@ -0,0 +1,31 @@
+import pywt
+import numpy as np
+import mne
+
+
+def cwt(X):
+    '''Compute the continuous wavelet transform of the input data.'''
+    
+    # epochs, ch, t
+    coefs, freqs = pywt.cwt(X, np.geomspace(
+        1, 70, num=64), wavelet='morl')  # TODO : try morl or cmor1.5-1.0
+    # freqs, epochs, ch, t
+    xtw = np.transpose(coefs, [1, 2, 0, 3])
+    # xtw[:,:] = np.abs(xtw[:,:,:-1, :-1])
+    xtw = np.abs(xtw)
+    return (xtw)
+
+def plot_pseudospectrogram(epochs: mne.Epochs):
+    X = epochs.get_data()
+    xtw = cwt(X)
+
+    axes = plt.subplot(111)
+    #
+    print(xwt.shape)
+    axes.pcolormesh(np.arange(xwt.shape[-1]), np.geomspace(1, 70, num=64), xwt[5,10], shading='nearest')
+    # Set yscale, ylim and labels
+    plt.yscale('log')
+    plt.ylim([1, 70])
+    plt.ylabel('Frequency (Hz)')
+    plt.xlabel('Time (sec)')
+    plt.show()
